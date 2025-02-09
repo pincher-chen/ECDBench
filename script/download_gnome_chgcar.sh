83a1164d446b9cb1ff5cd5f60bb7458e105f415f
Pin Chen <chenxupin@yeah.net>
2025-02-09 22:32:16 +0800

    Update download_gnome_chgcar.sh

--- script/download_gnome_chgcar.sh
+++ script/download_gnome_chgcar.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 for i in `cat ./filelist.txt`;do 
-    wget https://xx/chgcar/download/gnome/${i} # To maintain anonymity, we have hidden the download link. 
+    #wget https://xx/chgcar/download/gnome/${i} # To maintain anonymity, we have hidden the download link. 
+    wget https://matgen.nscc-gz.cn/chgcar/download/gnome/${i}
 done
