c73f2e1aacdda6c86524d5e1813b72a9961b9367
Pin Chen <chenxupin@yeah.net>
2025-02-09 22:34:08 +0800

    Update download_pbe_charge.sh

--- script/download_pbe_charge.sh
+++ script/download_pbe_charge.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 for i in `cat ./filelist.txt`;do 
-    wget https://xx/chgcar/download/pbe/${i}.tar.gz # To maintain anonymity, we have hidden the download link.
+    #wget https://xx/chgcar/download/pbe/${i}.tar.gz # To maintain anonymity, we have hidden the download link.
+    wget https://matgen.nscc-gz.cn/chgcar/download/pbe/${i}.tar.gz
 done
